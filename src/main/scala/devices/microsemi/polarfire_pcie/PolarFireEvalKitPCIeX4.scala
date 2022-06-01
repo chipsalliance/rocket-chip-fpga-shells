f24cf81e03f5d07db23786751c2e17d32bd86218
Henry Cook <henry@sifive.com>
2022-06-01 11:53:03 -0700

    append short boilerplate to all scala files

--- src/main/scala/devices/microsemi/polarfire_pcie/PolarFireEvalKitPCIeX4.scala
+++ src/main/scala/devices/microsemi/polarfire_pcie/PolarFireEvalKitPCIeX4.scala
@@ -73,3 +73,19 @@ class PolarFireEvalKitPCIeX4(implicit p: Parameters) extends LazyModule with Has
     TLScope.module.reset := ResetCatchAndSync(io.port.PCIE_1_TL_CLK_125MHz, reset)
   }
 }
+
+/*
+   Copyright 2016 SiFive, Inc.
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
