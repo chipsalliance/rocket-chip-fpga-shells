chipsalliance/rocket-chip-fpga-shells

commit 58bf5b37390dbcb2e21aa37e28f846fe921aec59

remove one line license reference from top of source files

src/main/scala/ip/xilinx/arty100tmig/arty100tmig.scala
@@ -1,4 +1,3 @@
-package sifive.fpgashells.ip.xilinx.arty100tmig
 
 import Chisel._
 import chisel3.experimental.{Analog,attach}

src/main/scala/shell/Util.scala
@@ -1,4 +1,3 @@
-package sifive.fpgashells.shell
 
 import chisel3._
 import chisel3.experimental.{Analog, attach}

commit 6ae20e36eb19d38d2a03835dd826f23214c02c77

fix a bug introduced in 58bf5b3

src/main/scala/ip/xilinx/arty100tmig/arty100tmig.scala
@@ -1,3 +1,4 @@
+package sifive.fpgashells.ip.xilinx.arty100tmig
 
 import Chisel._
 import chisel3.experimental.{Analog,attach}

src/main/scala/shell/Util.scala
@@ -1,3 +1,4 @@
+package sifive.fpgashells.shell
 
 import chisel3._
 import chisel3.experimental.{Analog, attach}