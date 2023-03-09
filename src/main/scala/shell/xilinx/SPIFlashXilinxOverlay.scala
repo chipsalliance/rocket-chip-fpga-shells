2055a3681305518f972d90f0841d42ed63c19642
Jiuyang Liu <liu@jiuyang.me>
2023-03-09 17:21:09 +0800

    remove util.Cat

--- src/main/scala/shell/xilinx/SPIFlashXilinxOverlay.scala
+++ src/main/scala/shell/xilinx/SPIFlashXilinxOverlay.scala
@@ -1,7 +1,6 @@
 package sifive.fpgashells.shell.xilinx
 
 import chisel3._
-import chisel3.util.Cat
 import freechips.rocketchip.diplomacy._
 import sifive.fpgashells.shell._
 import sifive.fpgashells.ip.xilinx._
